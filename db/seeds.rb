e1b1fd90e60cd33761c775dbb35bcc12e9b4c071
Daniela Sannino <daniela.sannino1@gmail.com>
2018-07-15 15:45:44 -0400

    added posts controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,10 @@ posts = Post.all
   )
 end
 
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "Unique title", body: "Unique body")
+ puts "#{Post.count}"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"

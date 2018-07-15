d35b7a19584695febe19a1cf1e101ac3a5cdb8a2
Daniela Sannino <daniela.sannino1@gmail.com>
2018-07-15 13:57:25 -0400

    added bootstrap and some css to welcome page

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -15,4 +15,11 @@ describe "GET about" do
   end
 end
 
+describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+      expect(response).to render_template("faq")
+    end
+  end
+
 end

b59e7d737d4c23906fa73774855d6a606ee723a1
Daniela Sannino <daniela.sannino1@gmail.com>
2018-07-15 22:00:12 -0400

    added posts to topic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
 require 'random_data'
 include RandomData
+# Create Topics
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
 
 # Create Posts
 50.times do
   Post.create!(
+    topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
@@ -33,6 +42,7 @@ end
  puts "#{Post.count}"
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 puts "#{Question.count} questions created"
